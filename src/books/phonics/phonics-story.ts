752579cca2095173310f0be9bcc3102c6fb65a99
Caleb Garratt <metal.carratt@gmail.com>
2024-01-09 21:57:22 +1300

    Add 'b' to phonics

--- src/books/phonics/phonics-story.ts
+++ src/books/phonics/phonics-story.ts
@@ -1,15 +1,13 @@
 import { Story } from "../../code/story-i";
 
-const AA = './books/phonics/Aa.jpg'
-
 export const story: Story = {
-    
+
 config: {
     letterSize: 'big'
 },
 frames: [
     {
-        bg: AA,
+        bg: './books/phonics/Aa.jpg',
         d: {
             en: 'A',
             kr: 'a',
@@ -51,6 +49,51 @@ frames: [
             kr: '화난 도끼',
             enAudio: new Audio( require('@/assets/books/phonics/005e.mp3') ),
             krAudio: new Audio( require('@/assets/books/phonics/005k.mp3') )
+        }
+    },
+    {
+        bg: './books/phonics/Bb.jpg',
+        d: {
+            en: 'B',
+            kr: 'b',
+            enAudio: new Audio( require('@/assets/books/phonics/006.mp3') ),
+            krAudio: new Audio( require('@/assets/books/phonics/006.mp3') )
+        }
+    },
+    {
+        bg: './books/phonics/bear.jpg',
+        d: {
+            en: 'big bear',
+            kr: '큰 곰',
+            enAudio: new Audio( require('@/assets/books/phonics/007e.mp3') ),
+            krAudio: new Audio( require('@/assets/books/phonics/007k.mp3') )
+        }
+    },
+    {
+        bg: './books/phonics/bus.jpg',
+        d: {
+            en: 'busy bus',
+            kr: '바쁜 버스',
+            enAudio: new Audio( require('@/assets/books/phonics/008e.mp3') ),
+            krAudio: new Audio( require('@/assets/books/phonics/008k.mp3') )
+        }
+    },
+    {
+        bg: './books/phonics/bat.jpg',
+        d: {
+            en: 'bouncing bat',
+            kr: '통통 튀는 박쥐',
+            enAudio: new Audio( require('@/assets/books/phonics/009e.mp3') ),
+            krAudio: new Audio( require('@/assets/books/phonics/009k.mp3') )
+        }
+    },
+    {
+        bg: './books/phonics/bricks.jpg',
+        d: {
+            en: 'blue and brown bricks',
+            kr: '파란색과 갈색 벽돌',
+            enAudio: new Audio( require('@/assets/books/phonics/010e.mp3') ),
+            krAudio: new Audio( require('@/assets/books/phonics/010k.mp3') )
         }
     }
 ]};
